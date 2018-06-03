867c871fe7b1992934bfce85ea7ed61ca062d1a6
k-koda <keisuke.physi@gmail.com>
2018-06-03 17:14:12 +0900

    finish Lesson4

---
+++ JavaTraining/Lesson4/Sample2.java
@@ -0,0 +1,16 @@
+public class Sample2{ //必ずファイル名と同じ名前
+  public static void main(String[] args){
+    //実行する処理はこの中で
+    //System.out.println(1 + 2);
+    //System.out.println(9 - 4);
+    //System.out.println(8 * 6);
+    //System.out.println(22 / 11);
+    //System.out.println(57 % 12);
+
+    //System.out.println( 3 + 4 * 2 );
+    //System.out.println((3 + 4)* 2 );
+
+    System.out.println("Hello" + "World!");
+    System.out.println("Hello" + " " + "World!");
+  }
+} 
---
+++ JavaTraining/Lesson4/Sample3.java
@@ -0,0 +1,8 @@
+public class Sample3{ //必ずファイル名と同じ名前
+  public static void main(String[] args){
+    //実行する処理はこの中で
+    int radius;
+    radius = 5;
+    System.out.println(radius * radius * 3.14);
+  }
+} 
